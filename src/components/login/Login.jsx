389f1090907fd0a647090d8600187fd299a42546
Aadib Kazi <adibkazi1106@gmail.com>
2024-06-25 15:48:39 +0530

    j

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,25 +1,17 @@
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 import { auth, db } from "../../lib/firebase";
 import { doc, setDoc, collection, query, where, getDocs } from "firebase/firestore";
 import upload from "../../lib/upload";
 
 const Login = () => {
-
-
-  
-  const [avatar, setAvatar] = useState({
+    const [avatar, setAvatar] = useState({
     file: null,
     url: "",
   });
-
   const [loading, setLoading] = useState(false);
-
   const handleAvatar = (e) => {
     if (e.target.files[0]) {
       setAvatar({
@@ -28,20 +20,14 @@ const Login = () => {
       });
     }
   };
-
   const handleRegister = async (e) => {
     e.preventDefault();
     setLoading(true);
     const formData = new FormData(e.target);
-
     const { username, email, password } = Object.fromEntries(formData);
-
-    // VALIDATE INPUTS
     if (!username || !email || !password)
       return toast.warn("Please enter inputs!");
     if (!avatar.file) return toast.warn("Please upload an avatar!");
-
-    // VALIDATE UNIQUE USERNAME
     const usersRef = collection(db, "users");
     const q = query(usersRef, where("username", "==", username));
     const querySnapshot = await getDocs(q);
@@ -49,12 +35,9 @@ const Login = () => {
       setLoading(false);
       return toast.warn("Select another username");
     }
-
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
-
       const imgUrl = await upload(avatar.file);
-
       await setDoc(doc(db, "users", res.user.uid), {
         username,
         email,
@@ -62,11 +45,9 @@ const Login = () => {
         id: res.user.uid,
         blocked: [],
       });
-
       await setDoc(doc(db, "userchats", res.user.uid), {
         chats: [],
       });
-
       toast.success("Account created! You can login now!");
     } catch (err) {
       console.log(err);
@@ -75,14 +56,11 @@ const Login = () => {
       setLoading(false);
     }
   };
-
   const handleLogin = async (e) => {
     e.preventDefault();
     setLoading(true);
-
     const formData = new FormData(e.target);
     const { email, password } = Object.fromEntries(formData);
-
     try {
       await signInWithEmailAndPassword(auth, email, password);
     } catch (err) {
@@ -92,7 +70,6 @@ const Login = () => {
       setLoading(false);
     }
   };
-
   return (
     <div className="login">
       <div className="item">
